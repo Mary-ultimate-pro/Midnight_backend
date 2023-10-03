f0846ac2716bdc4f7bf114e7e9e803fb1c56070c
MaryKwawu <may419316@gmail.com>
2023-10-03 23:20:46 +0000

    modified app.py

--- app.py
+++ app.py
@@ -140,74 +140,6 @@ player_schema = PlayerSchema()
 players_schema = PlayerSchema(many=True)
 
 
-class Winner(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50))
-    score = db.Column(db.Integer, nullable=True)
-    number = db.Column(db.Integer)
-    round_id = db.Column(db.Integer, db.ForeignKey(Round.id), nullable=True)
-    game_id = db.Column(db.Integer, db.ForeignKey(Game.id), nullable=True)
-    deck_id = db.Column(db.Integer, db.ForeignKey(Deck.id), nullable=True)
-    is_player = db.Column(db.Boolean, default=False)
-
-    def __init__(self, name, score, number, round_id, game_id, deck_id, is_player):
-        self.name = name
-        self.score = score
-        self.number = number
-        self.round_id = round_id
-        self.game_id = game_id
-        self.deck_id = deck_id
-        self.is_player = is_player
-
-
-class WinnerSchema(ma.Schema):
-    class Meta:
-        fields = (
-            "name",
-            "score",
-            "number",
-            "round_id"
-            "game_id",
-            "deck_id",
-            "players",
-        )
-
-
-winner_schema = WinnerSchema()
-winners_schema = WinnerSchema(many=True)
-
-
-class Active(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50))
-    score = db.Column(db.Integer, nullable=True)
-    round_id = db.Column(db.Integer, db.ForeignKey(Round.id), nullable=True)
-    game_id = db.Column(db.Integer, db.ForeignKey(Game.id), nullable=True)
-    number = db.Column(db.Integer)
-
-    def __init__(self, name, score, number, round_id, game_id):
-        self.name = name
-        self.score = score
-        self.number = number
-        self.round_id = round_id
-        self.game_id = game_id
-
-
-class ActiveSchema(ma.Schema):
-    class Meta:
-        fields = (
-            "name",
-            "score",
-            "number",
-            "round_id",
-            "game_id",
-        )
-
-
-active_schema = ActiveSchema()
-actives_schema = ActiveSchema(many=True)
-
-
 @app.get("/")
 def home():
     return "Hello world!"
@@ -242,6 +174,20 @@ def get_cards():
 def get_card(id):
     card = Card.query.get(id)
     return card_schema.jsonify(card)
+
+
+@app.delete("/card/<id>")
+def delete_card(id):
+    Card.query.filter_by(id=id).delete()
+    db.session.commit()
+    return card_schema.jsonify(id)
+
+
+@app.patch("/card/<id>")
+def update_card(id):
+    Card.query.filter_by(id=id).update(request.json)
+    db.session.commit()
+    return card_schema.jsonify(Card.query.get(id))
 
 
 with app.app_context():
@@ -279,6 +225,20 @@ def get_player(id):
     return player_schema.jsonify(player)
 
 
+@app.delete("/player/<id>")
+def delete_player(id):
+    Player.query.filter_by(id=id).delete()
+    db.session.commit()
+    return player_schema.jsonify(id)
+
+
+@app.patch("/player/<id>")
+def update_player(id):
+    Player.query.filter_by(id=id).update(request.json)
+    db.session.commit()
+    return card_schema.jsonify(Player.query.get(id))
+
+
 @app.post("/game")
 def create_game():
     # id = request.json["id"]
@@ -305,6 +265,20 @@ def get_games():
 def get_game(id):
     game = Game.query.get(id)
     return game_schema.jsonify(game)
+
+
+@app.delete("/game/<id>")
+def delete_game(id):
+    Game.query.filter_by(id=id).delete()
+    db.session.commit()
+    return game_schema.jsonify(id)
+
+
+@app.patch("/game/<id>")
+def update_player(id):
+    Game.query.filter_by(id=id).update(request.json)
+    db.session.commit()
+    return Game_schema.jsonify(Game.query.get(id))
 
 
 @app.post("/deck")
@@ -334,6 +308,20 @@ def get_deck(id):
     return deck_schema.jsonify(deck)
 
 
+@app.delete("/deck/<id>")
+def delete_deck(id):
+    Deck.query.filter_by(id=id).delete()
+    db.session.commit()
+    return deck_schema.jsonify(id)
+
+
+@app.patch("/deck/<id>")
+def update_deck(id):
+    deck.query.filter_by(id=id).update(request.json)
+    db.session.commit()
+    return Deck_schema.jsonify(Deck.query.get(id))
+
+
 @app.post("/round")
 def create_round():
     # id = request.json["id"]
@@ -360,58 +348,15 @@ def get_round(id):
     return round_schema.jsonify(round)
 
 
-@app.post("/winner")
-def create_winner():
-    name = request.json["name"]
-    score = request.json["score"]
-    number = request.json["number"]
-    round_id = request.json["round_id"]
-    game_id = request.json["game_id"]
-    # is_active = request.json["is_active"]
-    deck_id = request.json["deck_id"]
-    is_player = request.json["is_player"]
-
-    new_winner = Winner(name, score, number, round_id, game_id, deck_id, is_player)
-    db.session.add(new_winner)
+@app.delete("/round/<id>")
+def round_deck(id):
+    Round.query.filter_by(id=id).delete()
     db.session.commit()
-
-    return winner_schema.jsonify(new_winner)
-
-
-@app.get("/winner")
-def get_winners():
-    winners = winners_schema.dump(Winner.query.all())
-    return jsonify(winners)
+    return round_schema.jsonify(id)
 
 
-@app.get("/winner/<id>")
-def get_winner(id):
-    winner = Winner.query.get(id)
-    return winner_schema.jsonify(winner)
-
-
-@app.post("/active")
-def create_active():
-    name = request.json["name"]
-    score = request.json["score"]
-    round_id = request.json["round_id"]
-    game_id = request.json["game_id"]
-    number = request.json["number"]
-
-    new_active = Active(name, score, number, round_id, game_id)
-    db.session.add(new_active)
+@app.patch("/round/<id>")
+def update_deck(id):
+    round.query.filter_by(id=id).update(request.json)
     db.session.commit()
-
-    return active_schema.jsonify(new_active)
-
-
-@app.get("/active")
-def get_actives():
-    actives = actives_schema.dump(Active.query.all())
-    return jsonify(actives)
-
-
-@app.get("/active/<id>")
-def get_active(id):
-    active = Active.query.get(id)
-    return active_schema.jsonify(active)
+    return Round_schema.jsonify(Round.query.get(id))
